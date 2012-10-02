f629d8dc90c17e5754ee11fdcd9e923d192fe515
Vernon <vern_go_123@yahoo.com>
2012-10-02 09:15:29 +0800

    Updated feature extraction and svm training and prediction to work with multiple instruments.

--- params.h
+++ params.h
@@ -2,12 +2,26 @@
 #define BUFFER_LEN 1024
 #define START_KEY 0
 #define NUM_KEYS 12
+#define NUM_DRUM_GROUPS 5
 #define NUM_PEAKS 20
+#define NUM_WAVS 1500
 
-#define NUM_WAVS 1000
-#define TRAINING_FILE "training"
-#define LABEL_FILE "label/"
+#define WINDOW_SIZE 1024
+#define HOP_SIZE 1024
+
+#define PIANO_TRAINING_FILE "piano_training"
+#define DRUM_TRAINING_FILE "drum_training"
+
+#define LABEL_DIR "label/"
+#define PIANO_LABEL_EXT "_0.label"
+#define DRUM_LABEL_EXT "_1.label"
+
 #define WAVE_FILE "wav/"
 
 #define TESTING_FILE "testing.txt"
 #define WAV_FILE "test.wav"
+
+#define MODEL_FILE_EXTENSION ".txt.model"
+#define TESTING_FILE "testing.txt"
+#define PIANO_OUTPUT_FILE "output0.txt"
+#define DRUM_OUTPUT_FILE "output1.txt"
